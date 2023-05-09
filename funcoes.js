cbba47a43e4cf92d833784fae628ad1cc3be537e
linuser1990 <linuxserver@hotmail.com>
2023-05-09 15:37:05 -0400

    antes de alterar clientes.ejs

--- funcoes.js
+++ funcoes.js
@@ -28,3 +28,17 @@ function getSelectedValue() {
  // alert(`Você selecionou: ${selectedValue}`);
 }
 
+
+//FORMATA CELULAR
+function formatarTelefone(telefone) {
+    // Remove tudo que não for dígito do número de telefone
+    let numeros = telefone.replace(/\D/g, '');
+    
+    // Adiciona os parênteses e o traço no número formatado
+    numeros = numeros.replace(/^(\d{2})(\d)/g, '($1)$2');
+    numeros = numeros.replace(/(\d{5})(\d)/, '$1-$2');
+    
+    // Retorna o número formatado
+    return numeros;
+  }
+
